8008a6bae2bfceb055bb039823cfef73f0ce305a
Ahmed Osama <142287501+Megawy@users.noreply.github.com>
2023-12-08 15:50:32 +0200

    Updata File Js

---
+++ js/Main.js
@@ -0,0 +1,15 @@
+function services() {
+  window.scrollTo(0, 900);
+}
+function projects() {
+  window.scrollTo(0, 1940);
+}
+function contact() {
+  window.scrollTo(0, 3270);
+}
+function up() {
+  window.scrollTo(0, 0);
+}
+function down() {
+  window.scrollBy(0, 800);
+}
